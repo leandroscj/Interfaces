1fb3717424d0ee74c9e26580f49c638ca0fb7b75
Leandro Sousa <leandroscjTI@gmail.com>
2020-09-11 16:57:42 -0300

    Programa interfaces

---
+++ Interfaces/Entities/Services/IOnlinePaymentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces.Entities.Services
+{
+    internal interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+
+        double Interest(double amount, int months);
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using Interfaces.Entities;
+using Interfaces.Entities.Services;
+using Interfaces.Services;
+using System;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
